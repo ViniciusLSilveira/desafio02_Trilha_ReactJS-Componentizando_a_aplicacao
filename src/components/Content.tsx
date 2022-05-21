e2a215cc0dfb724b8ed8c9d994191d6c21f29fb8
Vinícius de Lima Silveira <viniciuslsilveiradev@gmail.com>
2022-05-21 17:44:02 -0300

    Desafio concluido

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,54 @@
-export function Content() {
-  // Complete aqui
+import { useState, useEffect } from 'react';
+import { MovieCard } from './MovieCard';
+import { Header } from './Header';
+
+import { api } from '../services/api';
+
+import '../styles/content.scss';
+
+interface MovieProps {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+type ContentProps = {
+  selectedGenreId: number;
+};
+
+export function Content({ selectedGenreId }: ContentProps) {
+  const [movies, setMovies] = useState<MovieProps[]>([]);
+
+  useEffect(() => {
+    api
+      .get<MovieProps[]>(`movies/?Genre_id=${selectedGenreId}`)
+      .then((response) => {
+        setMovies(response.data);
+      });
+  }, [selectedGenreId]);
+
+  return (
+    <div className='container'>
+      <Header selectedGenreId={selectedGenreId} />
+
+      <main>
+        <div className='movies-list'>
+          {movies.map((movie) => (
+            <MovieCard
+              key={movie.imdbID}
+              title={movie.Title}
+              poster={movie.Poster}
+              runtime={movie.Runtime}
+              rating={movie.Ratings[0].Value}
+            />
+          ))}
+        </div>
+      </main>
+    </div>
+  );
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,31 @@
+import { useState, useEffect } from 'react';
+import { GenreResponseProps } from './SideBar';
+import { api } from '../services/api';
+
+import '../styles/header.scss';
+
+type HeaderProps = {
+  selectedGenreId: number;
+};
+
+export function Header({ selectedGenreId }: HeaderProps) {
+  const [selectedGenre, setSelectedGenre] = useState<GenreResponseProps>(
+    {} as GenreResponseProps
+  );
+
+  useEffect(() => {
+    api
+      .get<GenreResponseProps>(`genres/${selectedGenreId}`)
+      .then((response) => {
+        setSelectedGenre(response.data);
+      });
+  }, [selectedGenreId]);
+
+  return (
+    <header>
+      <span className='category'>
+        Categoria:<span> {selectedGenre.title}</span>
+      </span>
+    </header>
+  );
+}
